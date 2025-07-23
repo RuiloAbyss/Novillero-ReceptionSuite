b3f0f646dc6b15f1ed420e50a70af91eff88e7ec
RuiloAbyss <ruilo.abisai@outlook.com>
2025-07-23 16:08:07 -0700

    menu bar

--- Front/scripts/script.js
+++ Front/scripts/script.js
@@ -1,3 +1,10 @@
-function holamondo(){
-    console.log("hola do mondo");
+function loadHTML(file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('page-content').innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error al cargar el archivo:', error);
+    });
 }
